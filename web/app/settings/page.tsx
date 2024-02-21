5ea6a0e515c39fa741b54a830a4e38bc6f796aa0
thecooltechguy <subby@galiboo.com>
2024-02-20 19:20:25 -0800

    added civitai integration for model downloads that require login

---
+++ web/app/settings/page.tsx
@@ -0,0 +1,17 @@
+import { Nav } from "@/components/Nav";
+import SettingsUI from "@/components/SettingsUI";
+import { Config } from "@/lib/types";
+
+export default async function SettingsPage() {
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_COMFYUI_LAUNCHER_SERVER_URL}/get_config`);
+    const data = (await resp.json()) as Config;
+    return (
+        <main className="flex min-h-screen flex-col">
+            <div>
+                <Nav />
+            </div>
+
+            <SettingsUI initialConfig={data} />
+        </main>
+    );
+}
