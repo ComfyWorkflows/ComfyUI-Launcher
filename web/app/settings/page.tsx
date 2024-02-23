744df9f0a69351d5192503e6ab11c40f4206534a
thecooltechguy <subby@galiboo.com>
2024-02-22 21:31:32 -0800

    fixed docker build issue
    Former-commit-id: 6dc1e3d4731c085395c913d7f52faa72e91425ee

--- web/app/settings/page.tsx
+++ web/app/settings/page.tsx
@@ -2,6 +2,8 @@ import { Nav } from "@/components/Nav";
 import SettingsUI from "@/components/SettingsUI";
 import { Config } from "@/lib/types";
 
+export const dynamic = 'force-dynamic';
+
 export default async function SettingsPage() {
     const resp = await fetch(`${process.env.NEXT_PUBLIC_COMFYUI_LAUNCHER_SERVER_URL}/get_config`);
     const data = (await resp.json()) as Config;
