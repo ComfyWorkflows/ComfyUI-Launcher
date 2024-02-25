bd98ed6108b693d2cf109d6861c9c53a26891efa
thecooltechguy <subby@galiboo.com>
2024-02-25 14:43:02 -0800

    replacing next.js webapp with react.js
    Former-commit-id: 4f0c2eb3c5eea8a0d885aef8fa42903e198089b7

---
+++ web/src/main.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import IndexPage from './pages/index'
+import './index.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import ImportWorkflowPage from './pages/import/page';
+import NewWorkflowPage from './pages/new/page';
+import SettingsPage from './pages/settings/page';
+
+// Create a client
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <IndexPage />,
+  },
+  {
+    path: "/import",
+    element: <ImportWorkflowPage/>,
+  },
+  {
+    path: "/new",
+    element: <NewWorkflowPage/>,
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage/>,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
