fa83cce2649ccf5cd84dd4a194dbd108be25ef91
thecooltechguy <subby@galiboo.com>
2024-02-25 19:32:48 -0800

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-# check if a venv/ directory exists
-# if it doesn't, run: python3 -m venv venv
-# then run: source venv/bin/activate && pip install -r requirements.txt
 if [ ! -d "venv" ]; then
   echo "Creating virtual environment for ComfyUI Launcher...\n\n"
   python3 -m venv venv
@@ -8,7 +5,7 @@ if [ ! -d "venv" ]; then
 fi
 
 echo "Installing required packages...\n\n"
-source venv/bin/activate
+. venv/bin/activate
 pip install -r requirements.txt
 
 echo "\n\n"
