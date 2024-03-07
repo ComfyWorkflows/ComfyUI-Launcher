5488b61012672ca3b27e069cd8a666c18205adbb
K2adir <k2adir@gmail.com>
2024-03-07 18:59:20 -0500

    places SELECT buttons to the left side
    this tailwind edit, places the select button on the left side so the user can read and select the missing models easily. Before, the UI placed the select button on the all the way to the right, which makes it hard to recognize which select button belongs to the avalible module. Especially on wide screens.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,33 +1,37 @@
-import { Nav } from '../components/Nav';
-import WorkflowsGridView from '../components/WorkflowsGridView';
+import { Nav } from "../components/Nav";
+import WorkflowsGridView from "../components/WorkflowsGridView";
 
 function IndexPage() {
-    return (
-        <main className="flex min-h-screen flex-col">
-          <div>
-            <Nav />
-          </div>
-    
-          <div className="flex flex-row space-x-5 p-5">
-            <a href="/new">
-              <div className="hover:bg-gray-200 cursor-pointer flex flex-col w-fit h-fit rounded-md bg-gray-100 p-5 border border-gray-300">
-                <h1 className="text-lg font-semibold">Create workflow</h1>
-                <p className="mt-1 font-medium text-sm text-gray-500">Create a new ComfyUI project</p>
-              </div>
-            </a>
-            <a href="/import">
-              <div className="hover:bg-gray-200 cursor-pointer flex flex-col w-fit h-fit rounded-md bg-gray-100 p-5 border border-gray-300">
-                <h1 className="text-lg font-semibold">Import workflow</h1>
-                <p className="mt-1 font-medium text-sm text-gray-500">Run an existing workflow with zero setup</p>
-              </div>
-            </a>
-          </div>
-    
-          <div className="flex flex-col mt-5 p-5">
-            <WorkflowsGridView />
-          </div>
-        </main>
-      );
-    }
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div>
+        <Nav />
+      </div>
 
-export default IndexPage
+      <div className="flex flex-row space-x-5 p-5">
+        <a href="/new">
+          <div className="hover:bg-gray-200 cursor-pointer flex flex-col w-fit h-fit rounded-md bg-gray-100 p-5 border border-gray-300">
+            <h1 className="text-lg font-semibold">Create workflow</h1>
+            <p className="mt-1 font-medium text-sm text-gray-500">
+              Create a new ComfyUI project
+            </p>
+          </div>
+        </a>
+        <a href="/import">
+          <div className="hover:bg-gray-200 cursor-pointer flex flex-col w-fit h-fit rounded-md bg-gray-100 p-5 border border-gray-300">
+            <h1 className="text-lg font-semibold">Import workflow</h1>
+            <p className="mt-1 font-medium text-sm text-gray-500">
+              Run an existing workflow with zero setup
+            </p>
+          </div>
+        </a>
+      </div>
+
+      <div className="flex flex-col mt-5 p-5">
+        <WorkflowsGridView />
+      </div>
+    </main>
+  );
+}
+
+export default IndexPage;
