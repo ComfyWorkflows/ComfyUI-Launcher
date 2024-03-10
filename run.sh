aec153a5aa8db42773c79621af27e2de6b1d6fad
thecooltechguy <subby@galiboo.com>
2024-03-10 13:23:17 -0700

    wip

--- run.sh
+++ run.sh
@@ -13,4 +13,14 @@ pip install -r requirements.txt
 echo "\n\n"
 echo "ComfyUI Launcher is starting...\n\n"
 
+
+cd server/
+
+# start Celery worker in the bg
+celery -A server worker --loglevel=info &
+celery_worker_pid=$!
+
 cd server/ && python server.py
+
+# kill Celery worker when server.py is done
+kill $celery_worker_pid
